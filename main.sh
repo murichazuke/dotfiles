df16a5bf06cfc70e4295e32127f50a9ef18303e2
xiao <xiao@beproud.jp>
2022-04-13 17:17:35 +0900

    Install Ansible requirements in main.sh

--- main.sh
+++ main.sh
@@ -6,12 +6,17 @@ HERE=$(dirname "$(readlink -f "${0}")")
 VENV="${HERE}/venv"
 ANSIBLE="${VENV}/bin/ansible-playbook"
 ANSIBLE_GALAXY="${VENV}/bin/ansible-galaxy"
+PATH="${PATH}:~/.local/bin/"
 
+which pip || (
+    apt-get update && \
+    apt-get install --no-install-recommends -y python3 python3-distutils curl && \
+    (curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python3)
+)
 which virtualenv || pip install virtualenv
 test -d "${VENV}" || virtualenv "${VENV}"
 test -f "${ANSIBLE}" || "${VENV}/bin/pip" install ansible
 
 "${ANSIBLE_GALAXY}" install -r "${HERE}/requirements.yml"
-exec "${ANSIBLE}" -ilocalhost, -clocal "${HERE}/playbook.yml"
-
+exec "${ANSIBLE}" -ilocalhost, -clocal -K "${HERE}/playbook.yml" "${@}"
 
